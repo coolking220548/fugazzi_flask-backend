2526af3f9ccd886254831b73dd3a652edbc4c927
coolking220548 <brandonjnrmark57@gmail.com>
2025-09-23 11:47:11 +1000

    Fixed scrape_article

--- main.py
+++ main.py
@@ -122,10 +122,9 @@ def scrape_article(request: ArticleRequests):
         return {"text": text}
     else:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Referer": "https://google.com"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                        "(KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9"
         }
         print(url)
         response = requests.get(url, headers=headers)
